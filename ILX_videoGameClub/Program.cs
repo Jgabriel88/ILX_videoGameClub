26a0a06705e9ee65b73f9c353c8652c09460737c
JGabriel88 <j.gabriel.888@gmail.com>
2022-06-07 20:05:52 -0400

    Begginig the ILX Game Club app - Wish me luck

---
+++ ILX_videoGameClub/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * Variables
+ * platform - string
+ * gameName - string
+ * memberName - string
+ * isBorrowed - soolean
+ * dateBorrowed - dateTime()
+ * quantity - sbyte
+ * 
+ * Methods
+ * borrowGame(gameName, memberName)
+ * returnGame(gameName, memberName)
+ * addGame(gameName, quantity)
+ * removeGame(gameName, quantity)
+ * 
+ */
