592691468a7c28feac19ab698c5570595bdec053
JGabriel88 <j.gabriel.888@gmail.com>
2022-06-08 15:43:07 -0400

    declaring intital variables

--- ILX_videoGameClub/Program.cs
+++ ILX_videoGameClub/Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Intelex Video Game Club");
 
 /*
  * Variables
  * platform - string
  * gameName - string
  * memberName - string
+ * memberEmail - string
  * isBorrowed - soolean
  * dateBorrowed - dateTime()
  * quantity - sbyte
@@ -17,3 +17,12 @@ Console.WriteLine("Hello, World!");
  * removeGame(gameName, quantity)
  * 
  */
+
+string platform = "PS5";
+string gameName = "Horizon Forbiden West";
+string memberName = "Gabriel Silva";
+string memberEmail = "Gabriel.silva@intelex.com"
+bool isBorrowed = yes;
+DateTime dateBorrowed = new DateTime();
+sbyte quantity = 1;
+
