9dba36b6e43ad223283a247044a5b221d2c6f957
JGabriel88 <j.gabriel.888@gmail.com>
2022-06-08 16:03:18 -0400

    declaring MemberList Class

---
+++ ILX_videoGameClub/MemberList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILX_videoGameClub
+{
+    public class MemberList
+    {
+        string memberName;
+        string memberEmail;
+    }
+}
